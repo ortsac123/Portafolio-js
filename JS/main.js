97582934e6d3b75533a7f124420ff5da95a35107
rafael <rafakastro27@gmail.com>
2025-09-09 17:26:37 -0500

    Agregamso responside en la pagina inicio. Esta es la version inicial

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,16 @@
 //Aca ira la logica de navegacion interactiva 
+tsParticles.load("tsparticles", {
+  background: {
+    color: "#0d47a1" // azul oscuro de fondo
+  },
+  particles: {
+    number: { value: 60 },
+    size: { value: 3 },
+    move: { speed: 2 },
+    links: {
+      enable: true,
+      distance: 150,
+      color: "#ffffff"
+    }
+  }
+});
