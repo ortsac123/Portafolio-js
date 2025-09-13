87352aae12685bcb3069ad9d90480848d3d7316b
rafael <rafakastro27@gmail.com>
2025-09-13 07:13:18 -0500

    Se agrega estilo de linea con efecto hover

--- JS/main.js
+++ JS/main.js
@@ -2,9 +2,9 @@
 tsParticles.load("tsparticles", {
       background: { color: "#f3f2f2ff" },
       particles: {
-        number: { value: 40 },
+        number: { value: 70 },
         color: { value: "#690303ff" },
-        links: { enable: true, color: "#253bdfff", distance: 150 },
+        links: { enable: true, color: "#253bdfff", distance: 70 },
         move: { enable: true, speed: 2 }
       }
     });
