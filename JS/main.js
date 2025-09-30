b700e98e97d0a4e5a71364bf8324df69fc03fd10
rafael <rafakastro27@gmail.com>
2025-09-29 22:47:52 -0500

    Se agrega un favicon y opaco el color de los puntos del fonde de la pagina

--- JS/main.js
+++ JS/main.js
@@ -5,7 +5,7 @@ tsParticles.load("tsparticles", {
     background: { color: "#f3f2f2ff" },
     particles: {
         number: { value: 70 },
-        color: { value: "#690303ff" },
+        color: { value: "#e8c6c6ff" },
         links: { enable: true, color: "#253bdfff", distance: 70 },
         move: { enable: true, speed: 2 }
     }
